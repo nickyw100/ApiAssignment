dcbf2e54b553dd505af881960fd7c56ce116004e
Nick Wilson <32970459+nickyw100@users.noreply.github.com>
2021-03-31 09:56:52 -0600

    Added a comment for testing purposes

--- hibernate-demo/src/main/java/com/hibernate/hibernatedemo/HibernateDemoApplication.java
+++ hibernate-demo/src/main/java/com/hibernate/hibernatedemo/HibernateDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HibernateDemoApplication {
-
+// Start the Spring Boot Application
 	public static void main(String[] args) {
 		SpringApplication.run(HibernateDemoApplication.class, args);
 	}
